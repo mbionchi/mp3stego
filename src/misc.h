17f48b08238cf550920c94f1d23c63c2636055ae
Holly Tancredi <tancredi.holly@gmail.com>
2016-01-04 17:17:07 -0500

    remove trash

--- src/misc.h
+++
@@ -1,9 +0,0 @@
-#ifndef MP3STEGO_MISC_H
-#define MP3STEGO_MISC_H
-
-#include <stdio.h>
-
-void print_usage(FILE *, char *);
-void print_help(FILE *, char *);
-
-#endif
--- src/print_frame.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINT_FRAME_H
-#define PRINT_FRAME_H
-
-#include "mp3_frame.h"
-
-void print_frame_info(mp3_frame_t*, int);
-void print_head(mp3_frame_t*);
-
-#endif
